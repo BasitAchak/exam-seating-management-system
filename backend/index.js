3192e52c5629b5b041bae05d8540f30d7659cef0
BasitAchak <basitlateef52@gmail.com>
2025-12-14 11:08:30 +0500

    Update comment to clarify CI trigger

--- backend/index.js
+++ backend/index.js
@@ -23,5 +23,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Exam seating backend listening on port ${port}`);
 });
-// trigger backend c
+// trigger backend ci
 
